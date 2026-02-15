1fec4bb26e1685e8c1d4aacc1f66834753c081f1
Palle Westermann <pwe@tec.dk>
2026-02-15 12:29:32 +0100

    control flow done

---
+++ src/app/pages/control-flow/control-flow.ts
@@ -0,0 +1,24 @@
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-control-flow',
+  imports: [FormsModule],
+  templateUrl: './control-flow.html',
+  styleUrl: './control-flow.css',
+})
+export class ControlFlow {
+  isShowText : boolean = false;
+  showSuccessLabel: WritableSignal<boolean> = signal(false);
+  age = 0;
+  courses : string[] = ["Angular", "React"];
+
+  deptList = [
+    {name: "HR", id: 123},
+    {name: "IT", id: 444},
+  ];
+
+  ToggleLabelVisibility() {
+    this.showSuccessLabel.set(!this.showSuccessLabel());
+  }
+}
