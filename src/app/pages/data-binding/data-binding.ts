42f0c04477154e7997909718e996ea0818b82582
Palle Westermann <pwe@tec.dk>
2026-02-15 09:00:26 +0100

    First Commit

---
+++ src/app/pages/data-binding/data-binding.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css',
+})
+export class DataBinding {
+  deptName = "HR";
+  className = "danger";
+  inputType="radio";
+
+  showMessage() {
+    alert('Hello');
+  }
+
+  changeCity() {
+    alert('City changed');
+  }
+}
